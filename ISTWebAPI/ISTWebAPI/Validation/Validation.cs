02d6403e54f048ff7b66f0b3fefbeb2505d06195
Marko Dujović <dujovicm@outlook.com>
2022-08-28 20:19:44 +0200

    Update VAT Regex

--- ISTWebAPI/ISTWebAPI/Validation/Validation.cs
+++ ISTWebAPI/ISTWebAPI/Validation/Validation.cs
@@ -10,6 +10,7 @@ namespace ISTWebAPI.Validation
         private static readonly string phoneRegex = "\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|\r\n2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|\r\n4[987654310]|3[9643210]|2[70]|7|1)\\d{1,14}$";
         private static readonly string emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
         private static readonly string vatRegex = "^[A-Za-z]{2,4}(?=.{2,12}$)[-_\\s0-9]*(?:[a-zA-Z][-_\\s0-9]*){0,2}$";
+        private static readonly string vatRegexSimple = "[0-9]{9}";
         private static readonly string addressRegex = "([\\w \\.\\,])+";
         public static bool validatePreduzece(Preduzece p)
         {
@@ -19,7 +20,7 @@ namespace ISTWebAPI.Validation
                 {
                     if (Regex.IsMatch(p.email, emailRegex))
                     {
-                        if (Regex.IsMatch(p.vat, vatRegex))
+                        if (Regex.IsMatch(p.vat, vatRegexSimple))
                         {
                             if (Regex.IsMatch(p.companyAddress, addressRegex))
                             {
